c485e9e31dd11cec4bac09114087697bcc062d29
Sandro Turriate <sandro.turriate@gmail.com>
2017-07-17 16:35:47 -0700

    Cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,4 @@
 package main
 
 func main() {
-	/* urls := [1]string{ */
-	/*   "http://sfbay.craigslist.org/search/roo/sfc?&maxAsk=2500&nh=4&nh=10&nh=18&nh=21", */
-	/* } */
-	// urls := [2]string{
-	//   "http://sfbay.craigslist.org/search/roo/sfc?&maxAsk=2500&nh=4&nh=10&nh=18&nh=21",
-	//   "http://sfbay.craigslist.org/search/apa/sfc?maxAsk=2500&nh=4&nh=10&nh=18&nh=21",
-	// }
-	// channel := make(chan string)
-
-	// for _, url := range urls {
-	//   page := page_notify.Page{Url: url}
-	//   go page.CheckPeriodically(channel)
-	// }
-
-	// for {
-	//   select {
-	//   case <-channel:
-	//   }
-	// }
 }
